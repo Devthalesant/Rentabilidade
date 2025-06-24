27cfa50590925b58d93c4d58a38bc4b6b4f01a1c
Thales <tha.santoro2000@gmail.com>
2025-06-24 17:07:43 -0300

    Changing the menu Layout

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-import pandas as pd
-import numpy as np
-import os
-import glob
-from pathlib import Path
 import streamlit as st
-from Functions.dictionaries import obter_dicionarios
 from pages.analise_2024 import page_analyse_2024
-from pages.auth import login
 from pages.analise_2025 import page_analyse_2025
 
 st.set_page_config(
     page_title="Rentabilidade - Pró-Corpo", 
-    layout="wide"               
+    layout="wide",
+    menu_items=None               
 )
 
 def main():
